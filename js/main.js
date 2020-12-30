c7fd06e80a88bcfd0384f84a4ebc25b636ddd77c
Earl M. Smith <unixsa@gmail.com>
2020-12-30 18:47:46 -0500

    hide the image when the button is clicked

--- js/main.js
+++ js/main.js
@@ -6,11 +6,18 @@ const p1 = document.getElementById("p1");{
 
 const p2 = document.getElementById("p2");{
     p2.innerHTML = ("We are coders!");
-    document.getElementById("p1").style.color = "#004c54";
+    document.getElementById("p2").style.color = "#004c54";
+    document.getElementById('p2').style.font = "20px arial";
 }
 
 const img1 = document.createElement("IMG"); {
     img1.setAttribute("src", "images/SW-121117-Coder-Popup1.jpg");
+    img1.setAttribute("width", "500");
+    img1.setAttribute("height", "300");
+    img1.setAttribute("alt", "Image of Sample Code ");
     document.body.appendChild(img1);
 }
 
+removeItemButton.addEventListener('click', () => {
+    document.body.removeChild(img1);
+});
