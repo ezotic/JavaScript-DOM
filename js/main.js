08d0d30eb1fa725a3218ef15853ef883e4f62cb4
Earl M. Smith <unixsa@gmail.com>
2020-12-30 20:53:01 -0500

    added image

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 document.getElementById("bodyColor").style.fontFamily = "Arial, sans-serif";
 document.getElementById('listColor').style.color = "green";
+
+const img1 = document.createElement("IMG"); {
+    img1.setAttribute("src", "images/e.jpg");
+    img1.setAttribute("width", "250");
+    img1.setAttribute("height", "250");
+    img1.setAttribute("alt", "Image of Earl M. Smith");
+    document.body.appendChild(img1);
+}
